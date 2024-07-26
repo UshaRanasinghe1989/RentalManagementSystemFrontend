6320f875fefeb3656bb15a9f2e159e0a4733b8ec
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2024-07-26 17:56:52 +0530

    Customer form

--- rental-app/src/app/components/customer/add-customer-form/add-customer-form.component.ts
+++ rental-app/src/app/components/customer/add-customer-form/add-customer-form.component.ts
@@ -1,12 +1,28 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-customer-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './add-customer-form.component.html',
   styleUrl: './add-customer-form.component.css'
 })
 export class AddCustomerFormComponent {
+  constructor(private http:HttpClient){}
 
+  customer:any = {
+    customerName: undefined,
+    city: undefined,
+    contactNo: undefined
+  }
+
+  public addCustomer(customer:any){
+    this.http.post("http://localhost:8080/customer", customer).subscribe(
+      res => {
+        console.log(res);
+      }
+    )
+  }
 }
